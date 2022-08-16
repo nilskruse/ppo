48e6e47c3c3eba6e4f472b4ba25606940b88722c
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-08-16 17:37:10 +0200

    Some progress

--- ppo.py
+++ ppo.py
@@ -60,25 +60,58 @@ if __name__ == "__main__":
     env = gym.make("CartPole-v1")
     observation = env.reset()
     # collect rollout 
-    max_timesteps = 1024
+    max_timesteps = 128
     rollout_timesteps = 64
     total_timesteps = 0
     while(total_timesteps < max_timesteps):
+        # collect some data
         observations = []
         rewards = []
         actions = []
+        action_probs = []
+        values = []
+        gaes = []
 
-    for _ in range(10):
+        for _ in range(rollout_timesteps):
+            total_timesteps += 1
+            action_logits, action_sample, action_log_prob, value = policy(torch.from_numpy(observation))
+
+            observations.append(observation)
+            actions.append(action_sample.item())
+            action_probs.append(action_log_prob.item())
+            values.append(value)
+
+            act = action_sample.item()
+            observation, reward, done, info = env.step(act)
+
+            rewards.append(reward)
+
+            if done:
+                observation = env.reset()
+
+        # compute advantage estimates
+        gamma = 1.0
+        lambd = 1.0
+        for i in reversed(range(rollout_timesteps - 1)):
+            delta = rewards[i] + gamma * values[i + 1] - values[i]
+            gae = np.power(gamma * lambd, rollout_timesteps - i + 1)
+
+
+        # print(f"{observations}")
+        print(f"{rewards}")
+
+
+
+    # eval or something
+    for _ in range(1):
         env.render()
         print(f"{policy(torch.from_numpy(observation))}")
         action_logits, action_sample, action_log_prob, value = policy(torch.from_numpy(observation))
-
 
         print(f"action_sample={action_sample}")
         print(f"action_log_prob={action_log_prob}")
         # print(f"act={action}")
         print(f"value={value}")
-        
 
         act = np.argmax(action_logits.detach().numpy())
         observation, reward, done, info = env.step(act)
